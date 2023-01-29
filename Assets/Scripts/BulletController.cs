0d6ac0f4487f04cb9defb870b90ba890b1095c5e
Maxellmag <mariano.ggonzalez@gmail.com>
2023-01-29 16:10:59 -0300

    nuevos scripts
    nuevos cripts eta todo terminado menos el desafio de varios disparos con la j k etc porque ya disparo varias veces con space

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
-{   public float speed=10;
+{   //velocidad de la bala
+    public float speed=10;
+    //Direccion hacia adelante de la bala
     public Vector3 direction= Vector3.forward;
+    //Daño de la bala (no aplicado a nada)
     public float damage;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +17,7 @@ public class BulletController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {//movemos la bala en direccion "direction" a la velocidad de "speed" * time.deltatime
      transform.Translate(direction*speed*Time.deltaTime);   
     }
 }
--- Assets/Scripts/InstantiateGameobject.cs
+++ Assets/Scripts/InstantiateGameobject.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InstantiateGameobject : MonoBehaviour
-{   public GameObject ObjectToInstantiate;
+{   //creamos la referencia al gameobject a instanciar(el objeto que tenga el script se va a instanciar)
+    public GameObject ObjectToInstantiate;
+    public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,10 @@ public class InstantiateGameobject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      //si se detecta la presion de la tecla espacio se instancia una copia del gameobject(sin limite:pueden ser mil)
       if(Input.GetKeyDown(KeyCode.Space))
       {
+        //la instanciacion se hace en la posicion del objeto que tiene este script(SpawnBullet) y en su misma rotacion
         Instantiate(ObjectToInstantiate,transform.position,transform.rotation);
       }  
     }
