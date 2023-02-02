eb24390babd7d5d33af2dd38ba776453e740d1dc
Maxellmag <mariano.ggonzalez@gmail.com>
2023-02-01 22:56:49 -0300

    practico3
    temporizador y agrandar balas tocando space

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -9,6 +9,7 @@ public class BulletController : MonoBehaviour
     public Vector3 direction= Vector3.forward;
     //Daño de la bala (no aplicado a nada)
     public float damage;
+    public Vector3 scale;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,8 @@ public class BulletController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {//movemos la bala en direccion "direction" a la velocidad de "speed" * time.deltatime
+    {scale=gameObject.transform.localScale; 
+        //movemos la bala en direccion "direction" a la velocidad de "speed" * time.deltatime
      transform.Translate(direction*speed*Time.deltaTime);   
     }
 }
--- Assets/Scripts/InstantiateGameobject.cs
+++ Assets/Scripts/InstantiateGameobject.cs
@@ -5,27 +5,53 @@ using UnityEngine;
 public class InstantiateGameobject : MonoBehaviour
 {   //creamos la referencia al gameobject a instanciar(el objeto que tenga el script se va a instanciar)
     public GameObject ObjectToInstantiate;
-    public GameObject bullet;
+    
+    public float temporizador;
+    private float tiempoFaltante;
     // Start is called before the first frame update
     void Start()
     {
-        
+      ResetTimer(); 
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+    
+    Temporizador();
     BulletInstantiate();
     }
    
    
     void BulletInstantiate()
-    {
+    { if(tiempoFaltante<=0)
+      {
+        
+      ResetTimer();
+      ObjectToInstantiate.transform.localScale=new Vector3(0.3f,0.3f,0.3f);
+      //la instanciacion se hace en la posicion del objeto que tiene este script(SpawnBullet) y en su misma rotacion
+      Instantiate(ObjectToInstantiate,transform.position,transform.rotation);
+    }
         //si se detecta la presion de la tecla espacio se instancia una copia del gameobject(sin limite:pueden ser mil)
       if(Input.GetKeyDown(KeyCode.Space))
       {
-        //la instanciacion se hace en la posicion del objeto que tiene este script(SpawnBullet) y en su misma rotacion
-        Instantiate(ObjectToInstantiate,transform.position,transform.rotation);
-      }  
+        
+     ObjectToInstantiate.transform.localScale=new Vector3(2f,2f,2f);
+     Instantiate(ObjectToInstantiate,transform.position,transform.rotation);
+        
+      }  else
+      {
+        ObjectToInstantiate.transform.localScale=new Vector3(1f,1f,1f);
+      }
+    }
+    void Temporizador()
+    {
+      tiempoFaltante-=Time.deltaTime;
+      
+    }
+    void ResetTimer()
+    {
+      tiempoFaltante=temporizador;
     }
 }
