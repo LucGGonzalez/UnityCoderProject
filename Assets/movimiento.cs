84a59121f486017a7540440170a701c0ac133dab
Maxellmag <mariano.ggonzalez@gmail.com>
2023-03-17 00:34:17 -0300

    protorace

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class movimiento : MonoBehaviour
 {   public float giro;
@@ -12,21 +15,75 @@ public class movimiento : MonoBehaviour
     [SerializeField]Transform rueda1,rueda2,rueda3,rueda4;
     private float fuerzaFrenado=500f;
     private float actualFuerzaFrenado=0f;
-    private float velocidad=600f;
+    private float velocidad=500f;
     private float actualVelocidad=0;
+    public TextMeshProUGUI textoKmph;
+    public AudioSource emisor;    
+    public Camera Camara1;
+    public Camera Camara2;
+    float radiorueda= 0.33f;// radio de la rueda
+    
+    float rpmRueda; // rpm de la rueda
+    float circunFerencia; //circunferencia de la rueda
+     public TextMeshProUGUI tiempo;
+    private float tiempoPartida;
+    private float tiempoLLegada;
+    public bool tempActivo;
+    public AudioClip acelera;
+    public AudioClip idle;
+    float velocidadR;
+    public GameObject menuPrincipal;
+    [SerializeField]private bool pausaactiva;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale=0; 
+          pausaactiva=false;
+         emisor=GetComponent<AudioSource>();
+          emisor.loop=true;
+          emisor.clip=idle;
+          emisor.Play();
+       
        giro=0f;
        avanzo=0f;
+        tempActivo=false;
+      tiempoPartida=0;
+      tiempo.text=tiempoPartida.ToString("0000")+" Segundos";
+      Camara1.enabled=true;
+      Camara2.enabled=false;
+      
+      InitTextoContador();
+      
+      
     }
 
     // Update is called once per frame
     void Update()
     {   giro=Input.GetAxis("Horizontal");     
            
-         avanzo= Input.GetAxis("Vertical"); 
+         avanzo= Input.GetAxis("Vertical");
+          
+           if(tempActivo)
+        {
+            EmpezarTemporizador();
+        }  
+      if(Input.GetKeyDown(KeyCode.W))
+      {
+        emisor.loop=true;
+        emisor.clip=acelera;
+        emisor.Play();
+      }
+      if(Input.GetKeyUp(KeyCode.W))
+      {
+        emisor.loop=true;
+      emisor.clip=idle;
+      emisor.Play();
+      }
+      CambiarCamara(); 
+      InitTextoContador(); 
+      CambiarPausa();
+      
 
       
     }
@@ -53,6 +110,7 @@ public class movimiento : MonoBehaviour
        { if(avanzo!=0f)
            {
              Moverrueda(wheel1);
+             ObtenerRpmKmh(wheel1); 
              Moverrueda(wheel2);
              Moverrueda(wheel3);
              Moverrueda(wheel4);
@@ -95,4 +153,89 @@ public class movimiento : MonoBehaviour
 
 
          }
+         void CambiarCamara()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+           Camara1.enabled=false;
+           Camara2.enabled=true;
+           
+           
+           
+           }
+            if(Input.GetKey(KeyCode.X))
+            {
+                Camara1.enabled=true;
+                Camara2.enabled=false;
+                
+                
+            }
+           
+         
+    }
+      public void ObtenerRpmKmh(WheelCollider collider)
+    {   
+        rpmRueda=collider.rpm;
+        //Debug.Log(rpmRueda);
+        circunFerencia = 2.0f *( 3.14f * radiorueda); // Encontrar la circunferencia 2 Pi R
+        velocidadR = Mathf.Abs( (circunFerencia * rpmRueda)/60); // calcular km por hora
+       // Debug.Log("Kilometros por hora="+velocidadkmh);
+    }
+     void InitTextoContador()
+    {
+        textoKmph.text= "Velocidad: "+ Mathf.Abs(velocidadR).ToString("000")+"Km por hora";
+       
+    }
+     void EmpezarTemporizador()
+        {
+        if(tempActivo)
+        {
+            tiempoPartida+=Time.deltaTime;
+            
+            tiempo.text=tiempoPartida.ToString("0000")+" Segundos";
+            }
+        }
+        public void SalirJuego()
+        {
+          Application.Quit();
+        }
+        public void JugarJuego()
+        {
+          menuPrincipal.SetActive(false);
+          Time.timeScale=1; 
+          pausaactiva=false;
+          
+          
+          
+
+        }
+        void CambiarPausa()
+        {
+          if(Input.GetKeyDown(KeyCode.Escape))
+          {
+            if(pausaactiva==true)
+            {
+              ResumirJuego();
+            }else
+            {
+              PausarJuego();
+            }
+          }
+        }
+        void PausarJuego()
+        {
+          menuPrincipal.SetActive(true);
+          pausaactiva=true;
+          Time.timeScale=0; 
+          emisor.Stop(); 
+        }
+        void ResumirJuego()
+        {
+          menuPrincipal.SetActive(true);
+          Time.timeScale=1; 
+          pausaactiva=false;
+          emisor.loop=true;
+          emisor.clip=idle;
+          emisor.Play();
+        }
 }
